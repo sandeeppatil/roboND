3f27e05ce1508fc86ff4a83fa80fcbb11042b4e1
Sandeep Patil <reach.sandeep.patil@gmail.com>
2024-12-30 17:04:56 +0100

    Sample rospackage download and launch

---
+++ myros_ws/README.md
@@ -0,0 +1,7 @@
+# Package Dependencies Check
+```bash
+cd ~/my_ws
+source devel/setup.bash
+rosdep check simple_arm
+sudo apt install missing-package
+```
---
+++ myros_ws/build.sh
@@ -0,0 +1,6 @@
+catkin_make
+# Source the setup script to add the workspace to the ROS_PACKAGE_PATH
+source devel/setup.bash
+
+# To run
+# roslaunch simple_arm robot_spawn.launch
